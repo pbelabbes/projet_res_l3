f0b6764313eec6dac873e25e0c1dafdfb988702d
Pierre Belabbes <pbelabbes@gmail.com>
2017-12-05 16:49:04 +0100

    Serveur multi client. Coupe correctement mais pas encore de fin fils

--- serveur.c
+++ serveur.c
@@ -1,4 +1,4 @@
-/* auteurs soussi sirine, pierre baudriller , romain brunet , pierre belabbes  */
+﻿/*auteurs soussi sirine, pierre baudriller , romain brunet , pierre belabbes  */
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -25,13 +25,17 @@ int rendreService(int desc){
 
 	printf("%s\n","Prêt à rendre service" );
 	char recep[255];
-	
-	while(1){
-		read(desc, recep, 255);
-		printf("%s\n",recep);
+	int stop = 1;
+
+	while(stop > 0){
+		stop = read(desc, recep, 255);
+		if(stop) printf("%s\n",recep);
 	}
 
+	exit(0);
 }
+
+
 
 //Pour tester 'nc 127.0.0.1 27000'
 int main(int argc,char *argv[]){
