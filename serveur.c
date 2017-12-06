3ee494cac64884db4b8b8ee47b9e0e8d84dc8a26
Pierre Belabbes <pbelabbes@gmail.com>
2017-12-06 17:27:46 +0100

    serveur : envoie une réponse au client

--- serveur.c
+++ serveur.c
@@ -30,11 +30,12 @@ int rendreService(int desc){
 
 	while(stop > 0){
 		stop = read(desc, recep, 255);
-		if(stop) printf("%s\n",recep);
-
+		if(stop) printf("%s\n",recep); //1;params
+		//char* traiterRequete(char* requete);
 		//char* afficher_train( char* gare_depart, char* gare_arrivee, char* horaire);
 		//char* afficher_trains_tranche( char* gare_depart, char* gare_arrivee, char* horaire_depart, char* horaire_arrivee);
 		//char* afficher_horaires( char* gare_depart, char* gare_arrivee, int tri)	; ( trie : 0 -> pas de trie, 1 -> tri prix, 2 -> tri durée)
+		write(desc,recep,255);
 	}
 
 	exit(0);
